ca57992dc89154fe2b5c6b34f974d273e66103dc
thrinath <thrinathsunny99@gmail.com>
2022-11-12 12:45:34 +0530

    hello ass5

--- src/App.js
+++ src/App.js
@@ -1,25 +1,18 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import React from 'react'
+import {BrowserRouter,Routes,Route} from 'react-router-dom'
+import Base from './component/base'
+import Display from './display'
+import { useState } from 'react'
+const App = () => {
+  const[response,setResponse]=useState({})
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+   <BrowserRouter>
+   <Routes>
+    <Route path ="/" element={< Base setResponse={setResponse} />}></Route>
+    <Route path ="/specifcasteroid" element={<Display response={response}  />}></Route>
+   </Routes>
+   </BrowserRouter>
+  )
 }
 
-export default App;
+export default App
